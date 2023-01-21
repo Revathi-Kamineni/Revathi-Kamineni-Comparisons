ee0b9606c11a8a2b2f0a733c73429dce4f6a1d5c
Revathi-Kamineni <revathikamineni77@gmail.com>
2023-01-21 19:59:23 +0000

    completed lab Comparators

--- src/main/java/Comparators.java
+++ src/main/java/Comparators.java
@@ -24,8 +24,12 @@ public class Comparators {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
+        if(a==b){
+            return true;
+        }else{
         return false;
     }
+}
 
     /**
      * @param a the first number to be compared.
@@ -33,7 +37,11 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        return false;
+        if(a>b){
+            return true;
+        }else{
+            return false;
+        } 
     }
     /**
      * @param a the first number to be compared.
@@ -41,24 +49,36 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
+        if(a<b){
+            return true;
+        }else{
         return false;
     }
+}
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
+        if(a>=b){
+            return true;
+        }else{
         return false;
     }
+}
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
+        if(a<=b){
+            return true;
+        }else{
         return false;
     }
+}
 
     /**
      * @param a the first number to be compared.
@@ -66,6 +86,20 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
+        if(a!=b){
+            return true;
+        }else{
         return false;
     }
+}
+public static void main(String[] args){
+    Comparators c = new Comparators();
+    
+    System.out.println(c.isEqual(1, 1));
+    System.out.println(c.greaterThan(2, 1));
+    System.out.println(c.lessThan(1, 2));
+    System.out.println(c.greaterThanOrEqualTo(2, 1));
+    System.out.println(c.lessThanOrEqualTo(1, 2));
+    System.out.println(c.notEqualTo(1, 1));
+}
 }
