857afc5d92bc029a082d5e9165013d781e3bcc68
Revathi-Kamineni <Revathi-Kamineni@github.com>
2025-12-09 17:41:52 +0000

    Test run: 16/16 passed, 0 failed, 0 skipped - 2025-12-09T17:41:52.266Z

--- src/main/java/Comparators.java
+++ src/main/java/Comparators.java
@@ -25,11 +25,10 @@ public class Comparators {
      */
     public boolean isEqual(int a, int b){
         if(a==b){
-            return true;
-        }else{
-        return false;
+            return false;
+        }
     }
-    
+
 }
 
     /**
